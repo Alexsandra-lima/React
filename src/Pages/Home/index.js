b3a6b04f2af9efa24cd4559dcc2a305d17b5f474
Alexsandra <alexsandramlimam@gmail.com>
2020-05-29 16:56:21 -0300

    primeira confirmação

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
+
+function App(props) {
+    const history = useHistory();
+    const [usuario, setUsuario] = useState('');
+    const [erro, setErro] = useState(false);
+
+    function handlePesquisa() {
+        axios.get(`https://api.github.com/users/${usuario}/repos`)
+            .then(Response => {
+                const repositories = Response.data;
+                const repositoriesName = [];
+                repositories.map((repository) => {
+                    repositoriesName.push(repository.name);
+                });
+                localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+                history.push('/repositories');
+            })
+
+            .catch(err => {
+                setErro(true);
+            });
+
+    }
+    return (
+        <S.HomeContainer>
+            <S.Contente>
+                <S.Input ClassName="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+                <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+            </S.Contente>
+            {erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : '' }
+        </S.HomeContainer>
+    );
+}
+
+export default App;
+//[usuario, setUsuario]
